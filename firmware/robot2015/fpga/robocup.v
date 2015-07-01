c00e3ad0eadaddf7ed95db07fd42238e4ebce793
Jonathan <jjones646@gmail.com>
2015-07-01 16:40:23 -0400

    updating for hardware synthesis

--- firmware/robot2015/fpga/robocup.v
+++ firmware/robot2015/fpga/robocup.v
@@ -5,6 +5,10 @@
 
 module robocup(clock, h, phaseHInv, phaseLInv); //re-add "duty_cycle,"  as port 2 after done testing
 
+parameter ENC_COUNT_WIDTH = 15;
+parameter DUTY_CYCLE_EIDTH = 10;
+parameter HALL_COUNT_WIDTH = 7;
+
 input clock;
 input [2:0] h;
 //input [`DUTY_CYCLE_WIDTH - 1 : 0] duty_cycle; re-add this line when done testing //TODO make FPGA read dutycycle
@@ -13,16 +17,38 @@ wire [2:0] u;
 
 output [2:0] phaseHInv, phaseLInv;
 wire [2:0] phaseHInv, phaseLInv, phaseH, phaseL;
-reg [`DUTY_CYCLE_WIDTH - 1 : 0] tempDC = 10'h00F;
+reg [DUTY_CYCLE_WIDTH-1:0] duty_cycle = 10'h00F;
 
-//Motor driver IRS2336DS requires inverted phase driver input
-assign phaseHInv = phaseH; //Jon's driver  doesn't need inverted outputs
-assign phaseLInv = phaseL; //TODO remove inverted outputs
 
-Hall_Effect_Sensor hallEffectSensor (clock, h, u, z);
+// Not actually used
+reg [ENC_COUNT_WIDTH-1:0] enc_count;
+reg [HALL_COUNT_WIDTH-1:0] hall_count;
+reg fault;
 
-Phase_Driver phaseDriver1 (clock, (u[2] == 1) ? tempDC/*duty_cycle*/ : 0, z[2], phaseH[2], phaseL[2]);
-Phase_Driver phaseDriver2 (clock, (u[1] == 1) ? tempDC/*duty_cycle*/ : 0, z[1], phaseH[1], phaseL[1]);
-Phase_Driver phaseDriver3 (clock, (u[0] == 1) ? tempDC/*duty_cycle*/ : 0, z[0], phaseH[0], phaseL[0]);
+// Dummy registers for synthesis right now
+reg enable = 1'b1;
+reg counts_reset = 1'b0;
+reg enc = 2'b00;
+
+    BLDC_Motor #(
+        .MIN_DUTY_CYCLE             ( 0 ) ,
+        .MAX_DUTY_CYCLE             ( 1<<DUTY_CYCLE_WIDTH ) ,
+        .MAX_DUTY_CYCLE_COUNTER     ( 1<<DUTY_CYCLE_WIDTH ) ,
+        .ENCODER_COUNT_WIDTH        ( ENC_COUNT_WIDTH ) ,
+        .HALL_COUNT_WIDTH           ( HALL_COUNT_WIDTH )
+        reg fault;)
+        ) bldc_motor (
+        .clk            ( clock ) ,
+        .en             ( enable ) ,
+        .reset_counts   ( counts_reset ) ,
+        .duty_cycle     ( duty_cycle ) ,
+        .enc            ( enc ) ,
+        .hall           ( h ) ,
+        .phaseH         ( phaseHInv ) ,
+        .phaseL         ( phaseLInv ) ,
+        .enc_count      ( encoder_count ) ,
+        .hall_count     ( hall_count ) ,
+        .hall_fault     ( fault ) 
+    );
 
 endmodule
